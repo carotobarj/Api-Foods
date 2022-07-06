c524acaf14e17028b530c7ddd668bc0fcfef1d44
Caro Tobar J <106922571+GRUPO12PF@users.noreply.github.com>
2022-07-05 21:10:45 -0500

    deploy

--- api/src/db.js
+++ api/src/db.js
@@ -31,7 +31,7 @@ let sequelize =
         ssl: true,
       })
     : new Sequelize(
-        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`,
+        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/development`,
         { logging: false, native: false }
       );
 // const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`, {
