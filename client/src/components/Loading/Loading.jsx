c950f382182e11082432d3c71cceccaa0399e7de
Caro Tobar J <82724532+carotobarj@users.noreply.github.com>
2023-12-08 19:37:23 -0500

    Update Loading.jsx

--- client/src/components/Loading/Loading.jsx
+++ client/src/components/Loading/Loading.jsx
@@ -4,14 +4,8 @@ import s from './Loading.module.css';
 
 export default function Loading() {
   return (
-    <div className={s.loader}>
-      <span>L</span>
-      <span>O</span>
-      <span>A</span>
-      <span>D</span>
-      <span>I</span>
-      <span>N</span>
-      <span>G</span>
-    </div>
+    
+      <div className={s.loader}></div>
+   
   );
 }
