af66701e493f8d0bd3ca05a3639368ebd18c76ae
Caro Tobar J <82724532+carotobarj@users.noreply.github.com>
2023-12-08 19:44:11 -0500

    Update Loading.jsx

--- client/src/components/Loading/Loading.jsx
+++ client/src/components/Loading/Loading.jsx
@@ -5,7 +5,7 @@ import s from './Loading.module.css';
 export default function Loading() {
   return (
     
-      <div className={s.loader}></div>
+      <div className={s.clockloader}></div>
    
   );
 }
